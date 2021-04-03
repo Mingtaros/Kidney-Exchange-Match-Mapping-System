e1f3db640b1f2926ebcab4b4ef3e937df34e5526
Leonardo Wijaya <leonardow41@gmail.com>
2021-04-03 20:11:30 +0700

    Frontend: Material UI

--- frontend/kidney-paired-exchange-frontend/src/App.js
+++ frontend/kidney-paired-exchange-frontend/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+import { Button } from '@material-ui/core';
+import { withStyles } from "@material-ui/core/styles";
 import './App.css';
 import history from './Utils/history'
 
+const useStyles = (theme) => ({
+  root: {
+    margin: theme.spacing(1)
+  }
+});
+
 class App extends React.Component {
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="App">
-        <div className="Content">
-          <Button onClick={() => history.push('/Home')}>
+        <div className={classes.root}>
+          <Button 
+              variant="contained"
+              color="primary"
+              onClick={() => history.push('/home')}
+              className={classes.root}>
             Go to Home Page
           </Button>
-          <Button onClick={() => history.push('/Admin')}>
+          <Button
+              variant="contained"
+              color="primary"
+              onClick={() => history.push('/admin')}
+              className={classes.root}>
             Go to Administrator Page
           </Button>
         </div>
@@ -20,4 +38,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(useStyles)(App);
